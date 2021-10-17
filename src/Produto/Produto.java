53a79eb5d29b7932672ab341b4fe9be33bc30edf
Matheus de Holanda <31321889+matheusholannda@users.noreply.github.com>
2021-10-17 15:42:58 -0300

    Adicionado try catch, loop para percorrer os produtos informados pelo usuário e criadas as validações para quando o valor inserido for zero (0)

--- src/Produto/Produto.java
+++ src/Produto/Produto.java
@@ -1,112 +1,113 @@
 package Produto;
 
 public class Produto {
+
 	// Declarações de campo de instância
-		private int numeroItem;
-		private String nomeProduto;
-		private int quantidadeEstoque;
-		private double precoUnitario;
-		private boolean ativo = true;
+	private int numeroItem;
+	private String nomeProduto;
+	private int quantidadeEstoque;
+	private double precoUnitario;
+	private boolean ativo = true;
+	
+	/*
+	 * Construtor padrão que permitirá que o compilador inicialize os valores
+	 * padrão. OBS: é necessário informar o construtor vazio quando tem mais de um
+	 * construtor, caso contrário será obrigatório ao instanciar o objeto informar
+	 * todos os parâmetros do construtor com parâmetros. Quando não informado
+	 * explicitamente, o próprio compilador já entende que ele existe
+	 */
+	public Produto() {
+	} // fim construtor padrão
 
-		/*
-		 * Construtor padrão que permitirá que o compilador inicialize os valores
-		 * padrão. OBS: é necessário informar o construtor vazio quando tem mais de um
-		 * construtor, caso contrário será obrigatório ao instanciar o objeto informar
-		 * todos os parâmetros do construtor com parâmetros. Quando não informado
-		 * explicitamente, o próprio compilador já entende que ele existe
-		 */
-		public Produto() {
-		} // fim construtor padrão
+	/*
+	 * Sobrecarga do construtor com passagem de parâmetros para todos os
+	 * campos/variáveis da classe
+	 */
+	public Produto(int numeroItem, String nomeProduto, int quantidadeEstoque, double precoUnitario) {
+		this.numeroItem = numeroItem;
+		this.nomeProduto = nomeProduto;
+		this.quantidadeEstoque = quantidadeEstoque;
+		this.precoUnitario = precoUnitario;
+	} // fim construtor da classe com parâmetros
 
-		/*
-		 * Sobrecarga do construtor com passagem de parâmetros para todos os
-		 * campos/variáveis da classe
-		 */
-		public Produto(int numeroItem, String nomeProduto, int quantidadeEstoque, double precoUnitario) {
-			this.numeroItem = numeroItem;
-			this.nomeProduto = nomeProduto;
-			this.quantidadeEstoque = quantidadeEstoque;
-			this.precoUnitario = precoUnitario;
-		} // fim construtor da classe com parâmetros
+	// Métodos getters(acessores) & setters(mutadores)
 
-		// Métodos getters(acessores) & setters(mutadores)
+	/*
+	 * GET - retornam o valor da variável local (variável de classe)
+	 */
 
-		/*
-		 * GET - retornam o valor da variável local (variável de classe)
-		 */
+	/*
+	 * SET - atribuem um valor para a variável local através da passagem de
+	 * parâmetro do método. O tipo de dado na passagem de parâmetro do método deve
+	 * ser o mesmo tipo da variável local
+	 */
 
-		/*
-		 * SET - atribuem um valor para a variável local através da passagem de
-		 * parâmetro do método. O tipo de dado na passagem de parâmetro do método deve
-		 * ser o mesmo tipo da variável local
-		 */
+	public int getNumeroItem() {
+		return numeroItem;
+	} // método acessor que retorna o valor da variável numeroItem
 
-		public int getNumeroItem() {
-			return numeroItem;
-		} // método acessor que retorna o valor da variável numeroItem
+	public void setNumeroItem(int numeroItem) {
+		this.numeroItem = numeroItem;
+	} // método mutador que atribui valor à variável numeroItem com o valor passado
+		// por parâmetro do mesmo tipo da variável local
 
-		public void setNumeroItem(int numeroItem) {
-			this.numeroItem = numeroItem;
-		} // método mutador que atribui valor à variável numeroItem com o valor passado
-			// por parâmetro do mesmo tipo da variável local
+	public String getNomeProduto() {
+		return nomeProduto;
+	} // método acessor que retorna o valor da variável nomeProduto
 
-		public String getNomeProduto() {
-			return nomeProduto;
-		} // método acessor que retorna o valor da variável nomeProduto
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	} // método mutador que atribui valor à variável nomeProduto com o valor passado
+		// por parâmetro do mesmo tipo da variável local
 
-		public void setNomeProduto(String nomeProduto) {
-			this.nomeProduto = nomeProduto;
-		} // método mutador que atribui valor à variável nomeProduto com o valor passado
-			// por parâmetro do mesmo tipo da variável local
+	public int getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	} // método acessor que retorna o valor da variável quantidadeEstoque
 
-		public int getQuantidadeEstoque() {
-			return quantidadeEstoque;
-		} // método acessor que retorna o valor da variável quantidadeEstoque
+	public void setQuantidadeEstoque(int quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	} // método mutador que atribui valor à variável quantidadeEstoque com o valor
+		// passado por parâmetro do mesmo tipo da variável local
 
-		public void setQuantidadeEstoque(int quantidadeEstoque) {
-			this.quantidadeEstoque = quantidadeEstoque;
-		} // método mutador que atribui valor à variável quantidadeEstoque com o valor
-			// passado por parâmetro do mesmo tipo da variável local
+	public double getPrecoUnitario() {
+		return precoUnitario;
+	} // método acessor que retorna o valor da variável precoUnitario
 
-		public double getPrecoUnitario() {
-			return precoUnitario;
-		} // método acessor que retorna o valor da variável precoUnitario
+	public void setPrecoUnitario(double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	} // método mutador que atribui valor à variável precoUnitario com o valor passado
+		// por parâmetro do mesmo tipo da variável local
 
-		public void setPrecoUnitario(double precoUnitario) {
-			this.precoUnitario = precoUnitario;
-		} // método mutador que atribui valor à variável precoUnitario com o valor passado
-			// por parâmetro do mesmo tipo da variável local
+	public boolean isAtivo() {
+		return ativo;
+	} // método acessor para a variável booleana ativo
 
-		public boolean isAtivo() {
-			return ativo;
-		} // método acessor para a variável booleana ativo
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	} // método mutador que irá atribuir o valor true ou false para a variável local
 
-		public void setAtivo(boolean ativo) {
-			this.ativo = ativo;
-		} // método mutador que irá atribuir o valor true ou false para a variável local
+	// método para calcular o inventário do produto
+	public double getInventoryValue() {
+		return quantidadeEstoque * precoUnitario;
+	}
 
-		// método para calcular o inventário do produto
-		public double getInventoryValue() {
-			return quantidadeEstoque * precoUnitario;
+	public String toString() {
+		String descricaoProduto = "";
+
+		// variável que irá guardar o resultado amigável do produto ativo/descontinuado
+		String tempAtivo = "";
+
+		// Informar de forma amigável no console se o produto está ativo/descontinuado
+		if (this.ativo) {
+			tempAtivo = "Ativo";
+		} else {
+			tempAtivo = "Descontinuado";
 		}
 
-		public String toString() {
-			String descricaoProduto = "";
-
-			// variável que irá guardar o resultado amigável do produto ativo/descontinuado
-			String tempAtivo = "";
-
-			// Informar de forma amigável no console se o produto está ativo/descontinuado
-			if (this.ativo) {
-				tempAtivo = "Ativo";
-			} else {
-				tempAtivo = "Descontinuado";
-			}
-
-			return descricaoProduto = "---------- INFORMAÇÕES DO PRODUTO -----------\n" + "Número do Item \t\t : "
-					+ this.numeroItem + "\n" + "Nome \t\t\t : " + this.nomeProduto + "\n" + "Quantidade em estoque \t : "
-					+ this.quantidadeEstoque + "\n" + "Preço \t\t\t : " + this.precoUnitario + "\n"
-					+ "Valor do Estoque \t : " + getInventoryValue() + "\n"
-					+ "Status do Produto \t : " + tempAtivo + "\n" + "---------------------------------------------" + "\n";
-		}
+		return descricaoProduto = "---------- INFORMAÇÕES DO PRODUTO -----------\n" + "Número do Item \t\t : "
+				+ this.numeroItem + "\n" + "Nome \t\t\t : " + this.nomeProduto + "\n" + "Quantidade em estoque \t : "
+				+ this.quantidadeEstoque + "\n" + "Preço \t\t\t : " + this.precoUnitario + "\n"
+				+ "Valor do Estoque \t : " + getInventoryValue() + "\n"
+				+ "Status do Produto \t : " + tempAtivo + "\n" + "---------------------------------------------" + "\n";
+	}
 }
--- src/Produto/ProdutoTester.java
+++ src/Produto/ProdutoTester.java
@@ -1,102 +1,68 @@
 package Produto;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ProdutoTester {
 
 	public static void main(String[] args) {
 
-		// Instância de um novo produto
-		Produto cd = new Produto();
-
-		// Setando valores para as variáveis do produto
-		cd.setNumeroItem(1);
-		cd.setNomeProduto("Greatest Hits");
-		cd.setQuantidadeEstoque(25);
-		cd.setPrecoUnitario(9.99);
-
-		// Instância de um novo produto
-		Produto caderno = new Produto();
-
-		// Setando valores para as variáveis do produto
-		caderno.setNumeroItem(2);
-		caderno.setNomeProduto("Caderno de caligrafia");
-		caderno.setQuantidadeEstoque(32);
-		caderno.setPrecoUnitario(15.50);
-
-		// Instanciando novos produtos com parâmetros passados no construtos
-		Produto dvd = new Produto(3, "Beatles - Live in New York", 5, 25.50);
-		Produto discoVinil = new Produto(4, "The darkside of the moon", 100, 4.50);
-		Produto CDLinux = new Produto(5, "CD Linux Debian", 20, 1.00);
-		Produto CDWindows = new Produto(6, "CD Windows 10", 85, 250.49);
-		CDWindows.setAtivo(false); // setando o produto para "Descontinuado"
-
-		// Variável que irá solicitar entrada de dados via teclado
 		Scanner sc = new Scanner(System.in);
 
-		// variáveis temporárias para receber os valores digitados do produto
 		int tempNumber;
 		String tempName;
 		int tempQty;
 		double tempPrice;
 
-		/*
-		 * CADASTRO p1
-		 */
-		System.out.println("[CADASTRO p1]");
+		int maxSize = -1;
 
-		// Solicitando entrada de dados do usuário que irá armazenar em cada variável
-		System.out.print("Digite o número do produto: ");
-		tempNumber = sc.nextInt();
+		System.out.println("Insira o número de produtos que gostaria de adicionar");
 
-		System.out.print("Digite o nome do produto: ");
-		tempName = sc.next();
+		try {
+			do {
+				System.out.print("Insira 0 (zero) se não quiser adicionar produtos: ");
+				maxSize = sc.nextInt();
 
-		System.out.print("Digite a quantidade de produtos em estoque: ");
-		tempQty = sc.nextInt();
+				if (maxSize < 0) {
+					System.out.println("Valor incorreto inserido");
+				} else if (maxSize == 0) {
+					System.out.println("Não há produtos!");
+				} else {
 
-		System.out.print("Digite o valor unitário do produto: ");
-		tempPrice = sc.nextDouble();
+					Produto[] produtos = new Produto[maxSize];
 
-		// Instancia de Produto passando os parâmetros digitados pelo usuário para o
-		// construtor
-		Produto p1 = new Produto(tempNumber, tempName, tempQty, tempPrice);
-		System.out.println();
-		// FIM CADASTRO p1
+					for (int i = 0; i < produtos.length; i++) {
 
-		/*
-		 * CADASTRO p2
-		 */
-		System.out.println("[CADASTRO p2]");
-		// Solicitando entrada de dados do usuário que irá armazenar em cada variável
-		System.out.print("Digite o número do produto: ");
-		tempNumber = sc.nextInt();
+						// sc.close(); -- IllegalStateException
 
-		System.out.print("Digite o nome do produto: ");
-		tempName = sc.next();
+						System.out.print("[ PRODUTO - " + (i + 1) + " ]\n");
 
-		System.out.print("Digite a quantidade de produtos em estoque: ");
-		tempQty = sc.nextInt();
+						System.out.print("Número do produto: ");
+						tempNumber = sc.nextInt();
 
-		System.out.print("Digite o valor unitário do produto: ");
-		tempPrice = sc.nextDouble();
+						System.out.print("Nome: ");
+						tempName = sc.next();
 
-		sc.close();
+						System.out.print("Quantidade: ");
+						tempQty = sc.nextInt();
 
-		// Instancia de Produto passando os parâmetros digitados pelo usuário para o
-		// construtor
-		Produto p2 = new Produto(tempNumber, tempName, tempQty, tempPrice);
-		// FIM CADASTRO p2
+						System.out.print("Preço: R$ ");
+						tempPrice = sc.nextDouble();
 
-		// saída dos valores do produto informado no console
-		System.out.println();
-		System.out.println(cd);
-		System.out.println(dvd);
-		System.out.println(discoVinil);
-		System.out.println(CDLinux);
-		System.out.println(CDWindows);
-		System.out.println(p1);
-		System.out.println(p2);
+						produtos[i] = new Produto(tempNumber, tempName, tempQty, tempPrice);
+
+					}
+					// mostrar os produtos inseridos anteriormente.
+					for (Produto produto : produtos) {
+						System.out.println(produto);
+					}
+				}
+			} while (maxSize < 0);
+		} catch (InputMismatchException e) {
+			System.out.println("Tipo incorreto de dados inserido!");
+			e.printStackTrace();
+			sc.close();
+		}
+
 	}
-
 }
